4954d668ef5c142ec126916bd0eb0a24bd02f5e9
shaikjani-16 <mahaboobjanishaik2002@gmail.com>
2025-01-31 01:01:18 +0530

    token_update

--- contracts/Creating_Token-ERC20/token_mint.sol
+++ contracts/Creating_Token-ERC20/token_mint.sol
@@ -1,42 +1,43 @@
-//SPDX-License-Identifier:MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract CreateToken{
+contract CreateToken {
     address public owner;
-    mapping(address=>uint) balances;
-    uint public supply =0;
-    constructor(){
-        owner=msg.sender;
+    mapping(address => uint) private balances;
+    uint public supply = 0;
+
+    constructor() {
+        owner = msg.sender;
     }
 
-    modifier isOWner(){
-        require(msg.sender==owner,"Unauthorized");
+    modifier isOwner() {
+        require(msg.sender == owner, "Unauthorized");
         _;
     }
 
-    function createMint(uint _noOfTokens) public isOWner  returns (string memory){
-        balances[owner]=_noOfTokens;
-        supply+=_noOfTokens;
-        return "Sucessfully minted";
-    }
-    function getBalance() public view returns(uint){
-        return balances[msg.sender];
-    }
-    function mintTo(address payable _addr,uint _amount) public isOWner returns(string memory){
-        balances[_addr]=_amount;        
-        balances[owner]= supply-_amount;
-        supply=supply-_amount;
-        return "Minted To";
-    }
-    function transfer(address payable _toAddress,uint _amount) public returns (string memory){
-        require(balances[msg.sender]>=_amount,"Insufficient balance");
-        balances[msg.sender]= balances[msg.sender]-_amount;
-        balances[_toAddress]+=_amount;
-        return "Sucessfully transfered";
+    function createMint(uint _noOfTokens) public isOwner returns (string memory) {
+        balances[owner] += _noOfTokens;
+        supply += _noOfTokens;
+        return "Successfully minted";
     }
 
+    function balanceOf(address _addr) public view returns (uint) {
+        return balances[_addr];
+    }
 
+    function mintTo(address payable _addr, uint _amount) public isOwner returns (string memory) {
+        require(_amount <= balances[owner], "Not enough supply");
+        balances[_addr] += _amount;
+        balances[owner] -= _amount;
+        return "Minted successfully";
+    }
 
-
-
+    function transfer(address _toAddress, uint _amount) public returns (string memory) {
+        require(balances[msg.sender] >= _amount, "Insufficient balance");
+        unchecked {
+            balances[msg.sender] -= _amount;
+            balances[_toAddress] += _amount;
+        }
+        return "Successfully transferred";
+    }
 }
